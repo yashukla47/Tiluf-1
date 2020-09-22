d456c31d911badb9b7d2494c19712718d2916b13
Chimbili-Pavan <53129750+Chimbili-Pavan@users.noreply.github.com>
2020-09-23 01:17:24 +0530

    modified according to the comments except history

--- SysAdminProxy.sol
+++ SysAdminProxy.sol
@@ -62,6 +62,28 @@ contract SysAdmin {
        admin = msg.sender;
    }
    
+   // function to create the user
+    // **** use msg.sender and check if the public address is the msg.sender -
+    // **** In registerUserName and registerUserNameAddr should not send sysadmincontractAddress as the argument -
+    // **** cannot send publickey as the aregument -
+    // **** cannot register a user from here -
+    function createAccount(string memory _userName, uint _role) public {
+        
+        require(getUserNameAddr(_userName) == msg.sender, " The username and address are not matching ");
+        
+        registerUserName(_userName);
+        
+        registerUserNameAddr(_userName);
+
+        mapAddToUserName(_userName);
+        
+        setRole(_userName,_role);
+        
+        // emit the event to store the new user details in the graph
+        //emit NewAccount(_userName, _name, _publicKey, _picHash, _descrip_ContactHash,_role);
+    }
+    
+   
    // fetch the user detials and return
    function isUserNameExists(string memory _userName) view public returns(bool){
        return userNames[_userName];
@@ -79,6 +101,7 @@ contract SysAdmin {
    }
    
    // set the user detials
+   // **** should be != address(0)
    function registerUserNameAddr(string memory _userName) public returns(bool){
        require(userNameAddress[_userName] == address(0), "  User already registered with this ethereum address");
        userNameAddress[_userName] = msg.sender;
@@ -87,6 +110,7 @@ contract SysAdmin {
    
    
    // check whether the address exists or not
+ 
    function isAddressExists(address _add) view public returns(bool){
        if(users[_add].length != 0){
            return true;
@@ -97,7 +121,9 @@ contract SysAdmin {
    }
    
    // set the user detials
-   function mapAddToUserName(string memory _userName, address _add) public {
+   // **** check if the username exists or not -- change the comment --- the default value of the bool in solidity is false.
+   // din understand the above comment - chimbili
+   function mapAddToUserName(string memory _userName) public {
       require(userNameAddress[_userName] == msg.sender, "  UserName has not mapped to the sender address ");
       require(userNames[_userName] == true, "  UserName has to first register on the chain ");
       
@@ -115,7 +141,7 @@ contract SysAdmin {
       users[msg.sender].push(_userName);
    }
    
-   // 
+   // **** why to keep this
    function setContractAddress(address _add) public returns(bool){
        require(msg.sender == admin, " The user is not the admin, so cannot set the contract addresses ");
        contractAddresses[_add] = true;
@@ -123,7 +149,7 @@ contract SysAdmin {
    }
    
    
-   // -> check wehther the version exists or not before
+   // **** check whether the version exists or not before -
    function setAssetVersionAddress(string memory ver,address _add) public returns(bool){
        require(msg.sender == admin, " The user is not the admin, so cannot set the asset contract version ");
        require(assetVer[ver] != _add, " The version already exists ");
@@ -132,7 +158,7 @@ contract SysAdmin {
    }
    
    
-   // -> check wehther the version exists or not before
+   // **** check wehther the version exists or not before -
    function setUserVersionAddress(string memory ver,address _add) public returns(bool){
        require(msg.sender == admin, " The user is not the admin, so cannot set the asset contract version ");
        require(userVer[ver] != _add, " The version already exists ");
@@ -152,36 +178,39 @@ contract SysAdmin {
       return userVer[ver];
    }
    
-   // 
+   // **** why to have this --- cannot be changed later on  ---- what is the contractAddresses for - 
    function setRole(string memory _userName,uint _role) public returns(bool){
-       require(contractAddresses[msg.sender] == true," The eth address is not registered in the eco-system ");
+       require(userNameAddress[_userName] == msg.sender, "  UserName has not mapped to the sender address ");
        role[_userName] = _role;
    }
    
-   //
+      // **** why to have this --- cannot be changed later on  ---- what is the contractAddresses for - 
    function getRole(string memory _userName) view public returns(uint){
-       require(contractAddresses[msg.sender] == true," The eth address is not registered in the eco-system ");
+       // require(contractAddresses[msg.sender] == true," The eth address is not registered in the eco-system ");
        uint _role = role[_userName];
        return _role;
    }
    
-   //
+   // 
    function getUserName(address _add) public view returns(string[] memory){
        return users[_add];
    }
    
-   //
+   // **** should we keep the products global ---- mostly not 
+   // **** not checking if the priduct is owned by the msg.sender 
    function setProductOwner(string memory _userName, string memory _productID) public returns(bool) {
+       
+       require(contractAddresses[msg.sender] == true, " The contract is not registered in the network ");
        productOwner[_productID] = _userName;
-       return true;
    }
    
-   //
+   // **** mostly will not keep this 
    function getProductOwner(string memory _productID) public view returns(string memory) {
       return productOwner[_productID];
    }
    
    // to add the new category
+   // **** default value for bool is false 
    function addCategory(string memory _category) public returns(bool) {
        require(categories[_category] != true, " The category already exists " ) ;
        categories[_category] = true;
